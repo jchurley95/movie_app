6c12d30ade157692bc709bf9efb6073e0089f1ba
jchurley95 <jchurley95@gmail.com>
2017-08-29 16:51:26 -0400

    Full CRUD

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,14 +1,49 @@
 class FilmsController < ApplicationController
-
+    # index
     def index
-        @films = Film.all
+     @films = Film.all
+   end
+   
+   # new
+   def new
+     @film = Film.new
+   end
+   
+    # create
+    def create
+        @film = Film.create!(film_params)
+        redirect_to film_path(@film)
     end
-
-    def show
-    end
-
-    def new
-        @film = Film.new
-    end
-
-end
+   
+   #show
+   def show
+     @film = Film.find(params[:id])
+   end
+   
+   # edit
+   def edit
+     @film = Film.find(params[:id])
+   end
+   
+   
+   # update
+   def update
+     @film = Film.find(params[:id])
+     @film.update(film_params)
+   
+    redirect_to film_path(@film)
+   end
+   
+   # destroy
+   def destroy
+     @film = Film.find(params[:id])
+     @film.destroy
+   
+    redirect_to films_path
+   end
+   
+   private
+   def film_params
+     params.require(:film).permit(:title, :genre, :year, :synopsis, :picture)
+     end
+   end
